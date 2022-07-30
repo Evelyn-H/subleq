1da4e92dbf45a78d0c9fcb5ae87b73a577299f64
Evelyn-H <hobert.evelyn@gmail.com>
2022-07-30 16:33:40 -0500

    command line arguments!

--- src/Main/Program.fs
+++ src/Main/Program.fs
@@ -1,4 +1,5 @@
-﻿open Interpreter
+﻿open Argu
+open Interpreter
 
 let program = [
     (12, 12, 3)
@@ -21,6 +22,46 @@ let program = [
     (33, 10, 53)
 ]
 
-program
-|> compile
-|> run
+let hello () = 
+    program
+    |> compile
+    |> run
+
+let run (path: string) =
+    ()
+
+let compile (path: string) =
+    ()
+
+type Args = 
+    | [<Unique; CliPrefix(CliPrefix.None)>] Run of path: string
+    | [<Unique; First; CliPrefix(CliPrefix.None)>] Compile of path: string
+    | [<Unique>] Hello
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Run _ -> "specify a file"
+            | Compile _ -> "specify a file"
+            | Hello -> "run a hello world program"
+
+
+[<EntryPoint>]
+let main args =
+    let parser = ArgumentParser.Create<Args>(programName = "subleq")
+    
+    try
+        let results = (parser.ParseCommandLine args).GetAllResults()
+        if results.Length = 0 
+        then printfn "%s" (parser.PrintUsage())
+        else
+            results
+            |> List.iter (fun r ->
+                match r with
+                | Run path -> run path
+                | Compile path -> compile path
+                | Hello -> hello ())
+    with e ->
+        printfn "%s" e.Message
+
+    0
