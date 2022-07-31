43b4b5e21c359c3e374fe63f2e85e73c9f67f25c
Evelyn-H <hobert.evelyn@gmail.com>
2022-07-30 23:04:22 -0500

    indentation

--- src/Interpreter/Interpreter.fs
+++ src/Interpreter/Interpreter.fs
@@ -1,63 +1,63 @@
 ﻿module Interpreter
 
-    // core types
-    type Instruction = int * int * int
-    type Program = Instruction list
-    type MachineCode = int list // essentially a flattened version of `Program`
+// core types
+type Instruction = int * int * int
+type Program = Instruction list
+type MachineCode = int list // essentially a flattened version of `Program`
 
-    // memory-mapped io abstraction
-    type IOHandler = int -> int -> unit
+// memory-mapped io abstraction
+type IOHandler = int -> int -> unit
 
-    // default io handler
-    let io location value   = 
-        match location with
-        | 1 -> printf "%c" (char value)
-        | _ -> ()
+// default io handler
+let io location value   = 
+    match location with
+    | 1 -> printf "%c" (char value)
+    | _ -> ()
 
-    // an instance of a running program
-    type Machine = {
-        memory: int array
-        instruction: int
-        halted: bool
-        io: IOHandler
-    }
+// an instance of a running program
+type Machine = {
+    memory: int array
+    instruction: int
+    halted: bool
+    io: IOHandler
+}
 
-    module Machine =
-        // constructor
-        let initialize io (code: MachineCode) = { memory = Array.ofList code; instruction = 0; halted = false; io = io }
+module Machine =
+    // constructor
+    let initialize io (code: MachineCode) = { memory = Array.ofList code; instruction = 0; halted = false; io = io }
 
-        // io operations
-        let inline read sys location = if location >= 0 then sys.memory[location] else 0
-        let inline read3 sys location = read sys location, read sys (location + 1), read sys (location + 2)
-        let inline write sys location value = Array.set sys.memory location value
+    // io operations
+    let inline read sys location = if location >= 0 then sys.memory[location] else 0
+    let inline read3 sys location = read sys location, read sys (location + 1), read sys (location + 2)
+    let inline write sys location value = Array.set sys.memory location value
 
-        // run one instruction
-        let step sys = 
-            if sys.instruction < 0 then { sys with halted = true } 
-            else
-                let a, b, c = read3 sys sys.instruction
-                let _a = read sys a // value stored at memory[a]
+    // run one instruction
+    let step sys = 
+        if sys.instruction < 0 then { sys with halted = true } 
+        else
+            let a, b, c = read3 sys sys.instruction
+            let _a = read sys a // value stored at memory[a]
 
-                // write new value (or print if b < 0)
-                if b < 0
-                then sys.io -b _a
-                else write sys b (read sys b - _a)
+            // write new value (or print if b < 0)
+            if b < 0
+            then sys.io -b _a
+            else write sys b (read sys b - _a)
 
-                // jump if <= 0 else go to next instruction
-                if read sys b <= 0 
-                then { sys with instruction = c }
-                else { sys with instruction = sys.instruction + 3 }
+            // jump if <= 0 else go to next instruction
+            if read sys b <= 0 
+            then { sys with instruction = c }
+            else { sys with instruction = sys.instruction + 3 }
 
-        // run machine until it halts
-        let rec run sys =
-            if sys.halted then ()
-            else 
-                step sys
-                |> run
+    // run machine until it halts
+    let rec run sys =
+        if sys.halted then ()
+        else 
+            step sys
+            |> run
 
-    let compile (program: Program) : MachineCode = List.collect (fun (a, b, c) -> [ a; b; c ]) program
+let compile (program: Program) : MachineCode = List.collect (fun (a, b, c) -> [ a; b; c ]) program
 
-    let run (program: MachineCode) = 
-        program
-        |> Machine.initialize io
-        |> Machine.run
+let run (program: MachineCode) = 
+    program
+    |> Machine.initialize io
+    |> Machine.run
